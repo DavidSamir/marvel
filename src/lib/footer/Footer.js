00117b65ac6b74ab4a146cd786c7836425c95cb5
David Samir <david.samir.wd@gmail.com>
2020-08-02 08:26:19 +0200

    add footer

---
+++ src/lib/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.scss'
+
+
+const Footer = ()=> {
+    return ( 
+        <footer>
+            <div className="container">
+            Developed By <a href="davidsamir.com">David Samir</a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
